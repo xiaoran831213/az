490698a215a58b35b7462af4f68cd21c84f461f9
xtong <tongxia1@msu.edu>
2015-05-20 17:25:38 -0400

    continue to work on work.py

--- mri/work.py
+++ mri/work.py
@@ -3,6 +3,8 @@ import glob
 import os
 import csv
 import numpy as np
+import os.path as pt
+from scipy.sparse import dok_matrix
 
 ## csv to surface
 CSV = [
@@ -41,17 +43,44 @@ VOX = np.dtype([
     ('cnv', '<f4'),
     ('tck', '<f4')])
 
+def i_fns(fdr = "", ptn = "*"):
+    for f in glob.glob(pt.join(fdr, ptn)):
+        yield f
+
+def g_cnt(fdr = "", ptn = "*"):
+    return len(glob.glob(pt.join(fdr, ptn)))
+
+def i_pks(fdr = "", ptn = "*", ssn = False):
+    for fi in glob.glob(pt.join(fdr, ptn)):
+        sn = pt.basename(pt.splitext(fi)[0])
+        with open(fi, 'rb') as pk:
+            sf = cPickle.load(pk)
+        if ssn:
+            yield (sn, sf)
+        else:
+            yield sf
+
+def i_pos(fdr = "", ptn = "*", ssn = False):
+    for fi in glob.iglob(pt.join(fdr, ptn)):
+        sn = pt.basename(pt.splitext(fi)[0])
+        with open(fi, 'rb') as pk:
+            sf = cPickle.load(pk)
+        if ssn:
+            yield (sn, sf['pos'])
+        else:
+            yield sf['pos']
+
 def csv2npy(src, dst, ovr = False, flt = None):
     """ read raw csv into surface in 2D list """
     if not os.path.exists(dst):
         os.mkdir(dst)
 
     print "\ncsv2sfr: ", src, " -> ", dst
-    for fi in glob.glob(os.path.join(src, "*")):
-        sn = os.path.basename(os.path.splitext(fi)[0])
-        fo = os.path.join(dst, sn)
+    for fi in i_fns(src):
+        sn = os.path.basename(pt.splitext(fi)[0])
+        fo = pt.join(dst, sn)
         renew = False
-        if os.path.isfile(fo):
+        if pt.isfile(fo):
             if not ovr:
                 print fo, "exists"
                 continue
@@ -70,9 +99,7 @@ def csv2npy(src, dst, ovr = False, flt = None):
                 if flt == None or flt(v):
                     sf.append(v)
 
-        ## put the list into numpy array:
-        sf = np.array(sf, dtype = NPY)
-
+        sf = np.array(sf, dtype = NPY)   # list to np.array
         with open(fo, 'wb') as f:
             cPickle.dump(sf, f, cPickle.HIGHEST_PROTOCOL)
 
@@ -80,29 +107,43 @@ def csv2npy(src, dst, ovr = False, flt = None):
             print fo, "renewed"
         else:
             print fo, "created"
+
+BND = np.dtype([
+    ('ssn', np.str, 32),
+    ('min', np.float32, (3,)),
+    ('max', np.float32, (3,)),
+    ('len', np.float32, (3,))])
+
+def g_bnd(src):
+    """ get dataset coordinate bound """
+    s_m = []
+    for sn, ps in i_pos(src, ssn = True):
+        p_min = ps.min(axis = 0)
+        p_max = ps.max(axis = 0)
+        s_m.append((sn, p_min, p_max, p_max - p_min))
+    s_m = np.array(s_m, dtype = BND)
+    return s_m
     
-def pos2vox(src, dst, sz = 1, ovr = False):
-    if not os.path.exists(dst):
+def pos2vox(src, dst, vsz = 1, ovr = False):
+    if not pt.exists(dst):
         os.mkdir(dst)
 
-    print "\posf2vox: ", src, " -> ", dst
-    for fi in glob.glob(os.path.join(src, "*")):
-        sn = os.path.basename(os.path.splitext(fi)[0])
-        fo = os.path.join(dst, sn)
+    print "pos2vox: ", src, " -> ", dst
+    for sn, sf in i_pks(src, ssn = True):
+        fo = pt.join(dst, sn)
         renew = False
-        if os.path.isfile(fo):     # skip exists
+        if pt.isfile(fo):     # skip exists
             if not ovr:
                 print fo, "exists"
                 continue
             else:
                 renew = True
+        p_min = sf['pos'].min(axis = 0)
 
-        with open(fi, 'rb') as pk:
-            sf = cPickle.load(pk)
-
-        for v in sf:
-            v[3] = map(lambda e: int(round(e/sz)), v[3])
-
+        ## offset to 0, get voxel position
+        sf['pos'] = np.rint((sf['pos'] - p_min) / vsz)
+        sf['slc'] = sf['slc'] > -1
+        sf = np.array(sf, dtype = VOX)
         with open(fo, 'wb') as pk:
             cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
 
@@ -111,50 +152,82 @@ def pos2vox(src, dst, sz = 1, ovr = False):
         else:
             print fo, "created"
 
-def v_add(v1, v2):
-    """ add up to vertices """
-    return [
-        min(v1[0], v2[0]),
-        min(v1[1], v2[1]),
-        max(v1[2], v2[2]),
-        [   v1[3][0] + v2[3][0],
-            v1[3][1] + v2[3][1],
-            v1[3][2] + v2[3][2]],
-        v1[4] + v2[4],
-        v1[5] + v2[5],
-        v1[6] + v2[6],
-        v1[7] + v2[7],
-        v1[8] + v2[8],
-        v1[9] + v2[9]]
+def srt_vox(src, dst, ovr = False):
+    if not pt.exists(dst):
+        os.mkdir(dst)
+        
+    print "srt_vox: ", src, " -> ", dst
+    for sn, sf in i_pks(src, ssn = True):
+        fo = pt.join(dst, sn)
+        renew = False
+        if pt.isfile(fo):     # skip exists
+            if not ovr:
+                print fo, "exists"
+                continue
+            else:
+                renew = True
+        p_min = sf['pos'].min(axis = 0)
 
-def v_sum(v_grp):
-    return reduce(v_add, v_grp)
+        ## offset to 0, get voxel position
+        sf['pos'] = np.rint((sf['pos'] - p_min) / vsz)
+        sf['slc'] = sf['slc'] > -1
+        sf = np.array(sf, dtype = VOX)
+        with open(fo, 'wb') as pk:
+            cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
 
-def v_avg(v_grp):
-    v = v_sum(v_grp)
-    n = len(v_grp)
-    return [
-        v[0],
-        v[1], 
-        v[2],
-        [   v[3][0] / n,
-            v[3][1] / n,
-            v[3][2] / n],
-        v[4] / n,
-        v[5] / n,
-        v[6] / n,
-        v[7] / n,
-        v[8] / n,
-        v[9] / n]
+        if renew:
+            print fo, "renewed"
+        else:
+            print fo, "created"
+    
+VDM = (64, 64, 64)
+VLM = np.dtype([
+    ('ssn', '<i4'),
+    ('lbl', '<i2', VDM),
+    ('slc', '<u1', VDM),
+    ('crv', '<f4', VDM),
+    ('are', '<f4', VDM),
+    ('tdp', '<f4', VDM),
+    ('gdp', '<f4', VDM),
+    ('cnv', '<f4', VDM),
+    ('tck', '<f4', VDM)])
+
+def sfr2vlm(src, dst, dim, ovr = False):
+    if not pt.exists(dst):
+        os.mkdir(dst)
+
+    print "srf2vlm: ", src, " -> ", dst
+    for sn, sf in i_pks(src, ssn = True):
+        fo = pt.join(dst, sn)
+        renew = False
+        if pt.isfile(fo):     # skip exists
+            if not ovr:
+                print fo, "exists"
+                continue
+            else:
+                renew = True
+        p_min = sf['pos'].min(axis = 0)
+
+        ## offset to 0, get voxel position
+        sf['slc'] = sf['slc'] > -1
+        sf = np.array(sf, dtype = VOX)
+        with open(fo, 'wb') as pk:
+            cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
+        
+        if renew:
+            print fo, "renewed"
+        else:
+            print fo, "created"
+    
 
 def prt_pk(src, fs, ts = None, fv = 0, tv = None):
-    """ print pickle files"""
+    """ print pickle binary"""
     if ts == None:
         ts = fs + 1
     if tv == None:
         tv = fv + 5
         
-    for fi in glob.glob(os.path.join(src, "*"))[fs:ts]:
+    for fi in glob.glob(pt.join(src, "*"))[fs:ts]:
         print fi + ":"
         with open(fi, 'rb') as f:
             sf = cPickle.load(f)[fv:tv]
@@ -181,211 +254,14 @@ def get_pk(src, si = 0, fv = 0, tv = None):
     fv: from vertex
     tv: to vertex
     """
-    fi = glob.glob(os.path.join(src, "*"))[si]
+    fi = glob.glob(pt.join(src, "*"))[si]
     with open(fi, 'rb') as f:
         sf = cPickle.load(f)[fv:tv]
     return sf
         
-def csv2sfr(src, dst, ovr = False, flt = None):
-    """ read raw csv into surface in 2D list """
-    if not os.path.exists(dst):
-        os.mkdir(dst)
-
-    import csv
-    print "\ncsv2sfr: ", src, " -> ", dst
-    for fi in glob.glob(os.path.join(src, "*")):
-        sn = os.path.basename(os.path.splitext(fi)[0])
-        fo = os.path.join(dst, sn)
-        renew = False
-        if os.path.isfile(fo):
-            if not ovr:
-                print fo, "exists"
-                continue
-            else:
-                renew = True
-
-        fi = open(fi, 'rb')
-        reader = csv.reader(fi)
-        reader.next()             # skip header
-        sf = []
-        for line in reader:
-            v = [t(line[i]) for i, c, t in CSV]
-            if flt and not flt(v):
-                continue
-            sf.append(v)
-        fi.close()
-
-        with open(fo, 'wb') as pk:
-            cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
-
-        if renew:
-            print fo, "renewed"
-        else:
-            print fo, "created"
-
-def srf2vox(src, dst, ovr = False, sz = 2):
-    if not os.path.exists(dst):
-        os.mkdir(dst)
-
-    print "\nsrf2vox: ", src, " -> ", dst
-    for fi in glob.glob(os.path.join(src, "*")):
-        sn = os.path.basename(os.path.splitext(fi)[0])
-        fo = os.path.join(dst, sn)
-        renew = False
-        if os.path.isfile(fo):     # skip exists
-            if not ovr:
-                print fo, "exists"
-                continue
-            else:
-                renew = True
-
-        with open(fi, 'rb') as pk:
-            sf = cPickle.load(pk)
-
-        for v in sf:
-            v[3] = map(lambda e: int(round(e/sz)), v[3])
-
-        with open(fo, 'wb') as pk:
-            cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
-
-        if renew:
-            print fo, "renewed"
-        else:
-            print fo, "created"
-
-def vox_agr(src, dst, ovr = False):
-    """ aggregate vertices fall in the same voxel """
-    if not os.path.exists(dst):
-        os.mkdir(dst)
-
-    xyz_key = lambda v: v[3]
-    idx_key = lambda v: [v[1], v[0]]
-
-    print "\nvox2agr: ", src, " -> ", dst
-    for fi in glob.glob(os.path.join(src, "*")):
-        sn = os.path.basename(os.path.splitext(fi)[0])
-        fo = os.path.join(dst, sn)
-        renew = False
-        if os.path.isfile(fo):     # skip exists
-            if not ovr:
-                print fo, "exists"
-                continue
-            renew = True
-
-        with open(fi, 'rb') as pk: # read input
-            sf = cPickle.load(pk)
-
-        ## sort by voxel so vertices is grouped
-        sf.sort(key = xyz_key)
-        
-        ns = []                 # new surface
-        v = sf.pop()            # initlal vertex
-        grp = [v]               # initial voxel group
-        key = v[3]              # initial voxel key
-        while sf:
-            v = sf.pop()
-            if key == v[3]: 
-                grp.append(v)
-            else:               # see a new voxel
-                avg = v_avg(grp)
-                ns.append(v_avg(grp))
-                grp = [v]
-                key = v[3]
-        else:                   # deal with last voxel
-            ns.append(v_avg(grp))
-
-        ns.sort(key = idx_key)
-        with open(fo, 'wb') as pk:
-            cPickle.dump(ns, pk, cPickle.HIGHEST_PROTOCOL)
-
-        if renew:
-            print fo, "renewed"
-        else:
-            print fo, "created"
-
-def get_surface_bound(s):
-    max_x, max_y, max_z = (-32767,) * 3
-    min_x, min_y, min_z = (+32767,) * 3
-    for v in s:
-        x, y, z = v[3][0], v[3][1], v[3][2]
-        max_x, max_y, max_z = (
-            max(x, max_x),
-            max(y, max_y),
-            max(z, max_z))
-        min_x, min_y, min_z = (
-            min(x, min_x),
-            min(y, min_y),
-            min(z, min_z))
-    return (
-        (min_x, min_y, min_z),
-        (max_x, max_y, max_z))
-            
-def get_dataset_bound(src, m):
-    """ get coordinate boundary """
-    max_ = np.array((-32767,) * 3)
-    min_ = np.array((+32767,) * 3)
-    max_x, max_y, max_z = (-32767,) * 3
-    min_x, min_y, min_z = (+32767,) * 3
-    print "get_dataset_bound:", src
-    for fi in glob.glob(os.path.join(src, "*")):
-        sn = os.path.basename(os.path.splitext(fi)[0])
-
-        with open(fi, 'rb') as pk: # read input
-            sf = cPickle.load(pk)
-
-        sb = get_surface_bound(sf)
-        min_x = min(min_x, sb[0][0])
-        min_y = min(min_y, sb[0][1])
-        min_z = min(min_z, sb[0][2])
-        max_x = max(max_x, sb[1][0])
-        max_y = max(max_y, sb[1][1])
-        max_z = max(max_z, sb[1][2])
-
-        print sn, sb
-    return (
-        (min_x - m, min_y - m, min_z - m),
-        (max_x + m, max_y + m, max_z + m))
-
-def voffset(src, dst, offset, ovr = False):
-    """ offset voxels, report top, right, deap corner
-    """
-    if not os.path.exists(dst):
-        os.mkdir(dst)
-    print "aln2org: ", src, " -> ", dst
-
-    dx = -offset[0][0]                       # offset x
-    dy = -offset[0][1]                       # offset y
-    dz = -offset[0][2]                       # offset z
-
-    for fi in glob.glob(os.path.join(src, "*")):
-        sn = os.path.basename(os.path.splitext(fi)[0])
-        fo = os.path.join(dst, sn)
-        renew = False
-        if os.path.isfile(fo):     # skip exists
-            if not ovr:
-                print fo, "exists"
-                continue
-            else:
-                renew = True
-
-        with open(fi, 'rb') as pk: # read input
-            sf = cPickle.load(pk)
-
-        print get_surface_bound(sf), " -> ",
-        for v in sf:
-            v[3] = v[3][0] + dx, v[3][1] + dy, v[3][2] + dz
-        print get_surface_bound(sf)
-
-        with open(fo, 'wb') as pk:
-            cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
-
-        if renew:
-            print fo, "renewed"
-        else:
-            print fo, "created"
 
 def test():
-    csv2npy('dat/csv', 'dat/npy', ovr = 0)
+    csv2npy('dat/csv', 'dat/npy', ovr = 0, flt = lambda v: v[1] == 1011)
 #    csv2sfr("dat/csv", "dat/srf", ovr = 1, flt = lambda v: v[1] == 1026)
 #    srf2vox("dat/srf", "dat/vox", ovr = 1, sz = 2)
 #    vox_agr("dat/vox", "dat/agr", ovr = 1)
