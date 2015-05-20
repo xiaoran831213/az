3e188e943e5fb37f68ac19c71939cf6f36ebb883
xtong <tongxia1@msu.edu>
2015-05-20 08:55:13 -0400

    use numpy on work.py

--- mri/work.py
+++ mri/work.py
@@ -1,19 +1,115 @@
 import cPickle
 import glob
 import os
+import csv
+import numpy as np
 
 ## csv to surface
 CSV = [
     (0, "Index", int),
     (1, "Label", int),
     (2, "Sulcus", int),
-    (3, "coordinates", eval),
+    (3, "coordinates", lambda w: tuple(eval(w))),
     (4, "area", float),
     (5, "mean curvature", float),
     (6, "travel depth", float),
     (7, "geodesic depth", float),
     (8, "FreeSurfer convexity", float),
     (9, "FreeSurfer thickness", float)]
+
+NPY = np.dtype([
+    ('idx', '<i4'),
+    ('lbl', '<i2'),
+    ('slc', 'i1'),
+    ('pos', '<f4', (3,)),
+    ('crv', '<f4'),
+    ('are', '<f4'),
+    ('tdp', '<f4'),
+    ('gdp', '<f4'),
+    ('cnv', '<f4'),
+    ('tck', '<f4')])
+
+VOX = np.dtype([
+    ('idx', '<i4'),
+    ('lbl', '<i2'),
+    ('slc', 'i1'),
+    ('pos', 'u1', (3,)),
+    ('crv', '<f4'),
+    ('are', '<f4'),
+    ('tdp', '<f4'),
+    ('gdp', '<f4'),
+    ('cnv', '<f4'),
+    ('tck', '<f4')])
+
+def csv2npy(src, dst, ovr = False, flt = None):
+    """ read raw csv into surface in 2D list """
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+
+    print "\ncsv2sfr: ", src, " -> ", dst
+    for fi in glob.glob(os.path.join(src, "*")):
+        sn = os.path.basename(os.path.splitext(fi)[0])
+        fo = os.path.join(dst, sn)
+        renew = False
+        if os.path.isfile(fo):
+            if not ovr:
+                print fo, "exists"
+                continue
+            else:
+                renew = True
+
+        ## open vertices csv table, skip header
+        with open (fi, 'rb') as f:
+            vt = csv.reader(f)
+            vt.next()             
+
+            ## load vertex table into python list
+            sf = []
+            for line in vt:
+                v = tuple([t(line[i]) for i, c, t in CSV])
+                if flt == None or flt(v):
+                    sf.append(v)
+
+        ## put the list into numpy array:
+        sf = np.array(sf, dtype = NPY)
+
+        with open(fo, 'wb') as f:
+            cPickle.dump(sf, f, cPickle.HIGHEST_PROTOCOL)
+
+        if renew:
+            print fo, "renewed"
+        else:
+            print fo, "created"
+    
+def pos2vox(src, dst, sz = 1, ovr = False):
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+
+    print "\posf2vox: ", src, " -> ", dst
+    for fi in glob.glob(os.path.join(src, "*")):
+        sn = os.path.basename(os.path.splitext(fi)[0])
+        fo = os.path.join(dst, sn)
+        renew = False
+        if os.path.isfile(fo):     # skip exists
+            if not ovr:
+                print fo, "exists"
+                continue
+            else:
+                renew = True
+
+        with open(fi, 'rb') as pk:
+            sf = cPickle.load(pk)
+
+        for v in sf:
+            v[3] = map(lambda e: int(round(e/sz)), v[3])
+
+        with open(fo, 'wb') as pk:
+            cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
+
+        if renew:
+            print fo, "renewed"
+        else:
+            print fo, "created"
 
 def v_add(v1, v2):
     """ add up to vertices """
@@ -51,34 +147,45 @@ def v_avg(v_grp):
         v[8] / n,
         v[9] / n]
 
-def get_pk(src, f1, f2, l1=0, l2=5):
+def prt_pk(src, fs, ts = None, fv = 0, tv = None):
     """ print pickle files"""
-    for i, f in enumerate(glob.glob(os.path.join(src, "*"))):
-        if i < f1:
-            continue
-        if i >= f2:
-            break
-
-        print f + ":"
-        pk = open(f, 'rb')
-        sf = cPickle.load(pk)[l1:l2]
+    if ts == None:
+        ts = fs + 1
+    if tv == None:
+        tv = fv + 5
+        
+    for fi in glob.glob(os.path.join(src, "*"))[fs:ts]:
+        print fi + ":"
+        with open(fi, 'rb') as f:
+            sf = cPickle.load(f)[fv:tv]
         for v in sf:
-            v[0] = "{:4d}".format(v[0])
+            v = list(v)
+            v[0] = "{:5d}".format(v[0])
             v[1] = "{:4d}".format(v[1])
             v[2] = "{:3d}".format(v[2])
-            v[3] = map(lambda e: "{:7.3f}".format(e), v[3])
+            v[3] = map(lambda e: "{:7.2f}".format(e), v[3])
             v[3] = ",".join(v[3])
             v[4] = "{:5.2f}".format(v[4])
             v[5] = "{:5.2f}".format(v[5])
             v[6] = "{:6.4f}".format(v[6])
-            v[7] = "{:.2e}".format(v[7])
+            v[7] = "{:4.2e}".format(v[7])
             v[8] = "{:5.2f}".format(v[8])
             v[9] = "{:5.2f}".format(v[9])
             v = " ".join(v)
             print v
-        pk.close()
     print
     
+def get_pk(src, si = 0, fv = 0, tv = None):
+    """ get surface from pickle
+    si: surface index
+    fv: from vertex
+    tv: to vertex
+    """
+    fi = glob.glob(os.path.join(src, "*"))[si]
+    with open(fi, 'rb') as f:
+        sf = cPickle.load(f)[fv:tv]
+    return sf
+        
 def csv2sfr(src, dst, ovr = False, flt = None):
     """ read raw csv into surface in 2D list """
     if not os.path.exists(dst):
@@ -213,9 +320,10 @@ def get_surface_bound(s):
         (min_x, min_y, min_z),
         (max_x, max_y, max_z))
             
-    
-def get_dataset_bound(src):
+def get_dataset_bound(src, m):
     """ get coordinate boundary """
+    max_ = np.array((-32767,) * 3)
+    min_ = np.array((+32767,) * 3)
     max_x, max_y, max_z = (-32767,) * 3
     min_x, min_y, min_z = (+32767,) * 3
     print "get_dataset_bound:", src
@@ -235,21 +343,19 @@ def get_dataset_bound(src):
 
         print sn, sb
     return (
-        (min_x, min_y, min_z),
-        (max_x, max_y, max_z))
+        (min_x - m, min_y - m, min_z - m),
+        (max_x + m, max_y + m, max_z + m))
 
-def aln2org(src, dst, ovr = False, mrg = 2):
-    """ align voxels to the origin axis with margin
-    so no voxel will take nagative coordinates
+def voffset(src, dst, offset, ovr = False):
+    """ offset voxels, report top, right, deap corner
     """
     if not os.path.exists(dst):
         os.mkdir(dst)
     print "aln2org: ", src, " -> ", dst
 
-    bnd = get_dataset_bound(src)          # get bounds
-    dx = -bnd[0][0] + mrg                 # offset x
-    dy = -bnd[0][1] + mrg                 # offset y
-    dz = -bnd[0][2] + mrg                 # offset z
+    dx = -offset[0][0]                       # offset x
+    dy = -offset[0][1]                       # offset y
+    dz = -offset[0][2]                       # offset z
 
     for fi in glob.glob(os.path.join(src, "*")):
         sn = os.path.basename(os.path.splitext(fi)[0])
@@ -265,10 +371,10 @@ def aln2org(src, dst, ovr = False, mrg = 2):
         with open(fi, 'rb') as pk: # read input
             sf = cPickle.load(pk)
 
+        print get_surface_bound(sf), " -> ",
         for v in sf:
-            print v[3], " -> ",
             v[3] = v[3][0] + dx, v[3][1] + dy, v[3][2] + dz
-            print v[3]
+        print get_surface_bound(sf)
 
         with open(fo, 'wb') as pk:
             cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
@@ -277,19 +383,14 @@ def aln2org(src, dst, ovr = False, mrg = 2):
             print fo, "renewed"
         else:
             print fo, "created"
-    top = (
-        bnd[1][0] + dx + mrg,
-        bnd[1][1] + dy + mrg,
-        bnd[1][2] + dz + mrg)
-    print "new corner:", top, "\n"
-    return top
 
 def test():
+    csv2npy('dat/csv', 'dat/npy', ovr = 0)
 #    csv2sfr("dat/csv", "dat/srf", ovr = 1, flt = lambda v: v[1] == 1026)
-    srf2vox("dat/srf", "dat/vox", ovr = 1, sz = 2)
-    vox_agr("dat/vox", "dat/agr", ovr = 1)
+#    srf2vox("dat/srf", "dat/vox", ovr = 1, sz = 2)
+#    vox_agr("dat/vox", "dat/agr", ovr = 1)
 #    print get_dataset_bound("dat/agr")
-    aln2org("dat/agr", "dat/aln", ovr = 1, mrg = 0)
+#    voffset("dat/agr", "dat/aln", ovr = 1, offset = 0)
     
 if __name__ == "__main__":
     test()
