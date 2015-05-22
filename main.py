9d6982dc48bb2df91d6682d4c662e81ed92fea88
xtong <tongxia1@msu.edu>
2015-05-22 01:35:07 -0400

    delete older surface and dataset code, use work.py now. delete temporary work1.py; test work.py on whole data

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import csv
 import os
-import mri
+import mri.tar2csv as tar2csv
+import mri.work as work
+
+def test():
+    work.csv2npy('dat/csv', 'dat/npy', ovr = 0)
+    work.vtx2grd('dat/npy', 'dat/grd', ovr = 0, sz = 1)
+    work.srt_pos('dat/grd', 'dat/srt', ovr = 0)
+    work.cmb_pos('dat/srt', 'dat/cmb', ovr = 0)
+#    work.sfr2vlm('dat/cmb', 'dat/vlm', ovr = 0, dim = (64,)*3)
+
+def main():
+    pass
+    # tar2csv.tar2csv('raw/vtx.tar.gz', 'dat/csv')
+    
+if __name__ == "__main__":
+    test()
--- mri/work.py
+++ mri/work.py
@@ -19,22 +19,23 @@ CSV = [
     (8, "FreeSurfer convexity", float),
     (9, "FreeSurfer thickness", float)]
 
-F3D = np.dtype({
-    'names': ['x', 'y', 'z'],
-    'formats': [np.float32, np.float32, np.float32],
-    'itemsize': 96})
+F3D = np.dtype([
+    ('x', '<f4'),
+    ('y', '<f4'),
+    ('z', '<f4')])
+
 
 I3D = np.dtype([
     ('x', '<u1'),
     ('y', '<u1'),
     ('z', '<u1')])
 
-
+SHP = ('crv', 'are', 'tdp', 'gdp', 'cnv', 'tck')
 NPY = np.dtype([
     ('idx', '<i4'),
     ('lbl', '<i2'),
     ('slc', 'i1'),
-    ('pos', '<f4', (3,)),
+    ('pos', F3D),
     ('crv', '<f4'),
     ('are', '<f4'),
     ('tdp', '<f4'),
@@ -46,13 +47,19 @@ VOX = np.dtype([
     ('idx', '<i4'),
     ('lbl', '<i2'),
     ('slc', 'i1'),
-    ('pos', 'u1', (3,)),
+    ('pos', I3D),
     ('crv', '<f4'),
     ('are', '<f4'),
     ('tdp', '<f4'),
     ('gdp', '<f4'),
     ('cnv', '<f4'),
     ('tck', '<f4')])
+
+BND = np.dtype([
+    ('ssn', np.str, 32),
+    ('min', '<f4', (3,)),
+    ('max', '<f4', (3,)),
+    ('len', '<f4', (3,))])
 
 def i_fns(fdr = "", ptn = "*"):
     for f in glob.glob(pt.join(fdr, ptn)):
@@ -75,11 +82,11 @@ def i_pos(fdr = "", ptn = "*", ssn = False):
     for fi in glob.iglob(pt.join(fdr, ptn)):
         sn = pt.basename(pt.splitext(fi)[0])
         with open(fi, 'rb') as pk:
-            sf = cPickle.load(pk)
+            ps = cPickle.load(pk)['pos']
         if ssn:
-            yield (sn, sf['pos'])
+            yield (sn, ps)
         else:
-            yield sf['pos']
+            yield ps
 
 def csv2npy(src, dst, ovr = False, flt = None):
     """ read raw csv into surface in 2D list """
@@ -119,23 +126,22 @@ def csv2npy(src, dst, ovr = False, flt = None):
         else:
             print fo, "created"
 
-BND = np.dtype([
-    ('ssn', np.str, 32),
-    ('min', np.float32, (3,)),
-    ('max', np.float32, (3,)),
-    ('len', np.float32, (3,))])
-
 def g_bnd(src):
     """ get dataset coordinate bound """
     s_m = []
     for sn, ps in i_pos(src, ssn = True):
-        p_min = ps.min(axis = 0)
-        p_max = ps.max(axis = 0)
+        p_min = np.array(
+            (ps['x'].min(), ps['y'].min(), ps['z'].min()),
+            dtype = '<f4')
+        p_max = np.array(
+            (ps['x'].max(), ps['y'].max(), ps['z'].max()),
+            dtype = '<f4')
         s_m.append((sn, p_min, p_max, p_max - p_min))
     s_m = np.array(s_m, dtype = BND)
     return s_m
     
-def vtx2grd(src, dst, gsz = 1, ovr = False):
+def vtx2grd(src, dst, sz = 1, ovr = False):
+    """ vertex into grid """
     if not pt.exists(dst):
         os.mkdir(dst)
 
@@ -149,11 +155,12 @@ def vtx2grd(src, dst, gsz = 1, ovr = False):
                 continue
             else:
                 renew = True
-        p_min = sf['pos'].min(axis = 0)
 
         ## offset to 0, get voxel position
-        sf['pos'] = np.rint((sf['pos'] - p_min) / gsz)
-        sf['slc'] = sf['slc'] > -1
+        ps = sf['pos']          # reference, not a copy!
+        for a in 'xyz':
+            ps[a] = np.rint((ps[a] - ps[a].min()) / sz)
+
         sf = np.array(sf, dtype = VOX)
         with open(fo, 'wb') as pk:
             cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
@@ -179,8 +186,7 @@ def srt_pos(src, dst, ovr = False):
                 renew = True
 
         ## sort by position and save
-        p = sf['pos']
-        sf = sf[np.lexsort((p[:,2], p[:,1], p[:,0]))]
+        sf = sf[sf['pos'].argsort()]
         with open(fo, 'wb') as pk:
             cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
 
@@ -190,6 +196,9 @@ def srt_pos(src, dst, ovr = False):
             print fo, "created"
 
 def cmb_pos(src, dst, ovr = False):
+    """ combin vertices fall into the same grid,
+    the grid coordinates must be sorted first.
+    """
     if not pt.exists(dst):
         os.mkdir(dst)
         
@@ -204,28 +213,47 @@ def cmb_pos(src, dst, ovr = False):
             else:
                 renew = True
 
+        ## get unique position and starting indices
+        U, S = np.unique(sf['pos'], return_index = True)
+
+        ## container to hold the combined vertices
+        C = np.zeros(U.shape, sf.dtype)   
+
+        for k, g in enumerate(np.split(sf, S[1:])):
+            c = C[k]
+            c['idx'] = g['idx'].min();
+            c['lbl'] = g['lbl'].max();
+            
+            ## these features are averaged
+            for f in SHP:
+                c[f] = g[f].mean()
+
+            ## any vertex in Sulcus mean the whole group is in
+            c['slc'] = any(g['slc'] > -1)
+
+            ## and the position of the group is shared
+            c['pos'] = U[k]
+            
         with open(fo, 'wb') as pk:
-            cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
+            cPickle.dump(C, pk, cPickle.HIGHEST_PROTOCOL)
 
         if renew:
             print fo, "renewed"
         else:
             print fo, "created"    
 
-#b = np.ascontiguousarray(a).view(np.dtype((np.void, a.dtype.itemsize * a.shape[1])))
 VDM = (64, 64, 64)
 VLM = np.dtype([
-    ('ssn', '<i4'),
-    ('lbl', '<i2', VDM),
-    ('slc', '<u1', VDM),
-    ('crv', '<f4', VDM),
-    ('are', '<f4', VDM),
-    ('tdp', '<f4', VDM),
-    ('gdp', '<f4', VDM),
-    ('cnv', '<f4', VDM),
-    ('tck', '<f4', VDM)])
+    ('lbl', '<i2'),
+    ('slc', '<u1'),
+    ('crv', '<f4'),
+    ('are', '<f4'),
+    ('tdp', '<f4'),
+    ('gdp', '<f4'),
+    ('cnv', '<f4'),
+    ('tck', '<f4')])
 
-def sfr2vlm(src, dst, dim, ovr = False):
+def sfr2vlm(src, dst, dim = (64,)*3, ovr = False):
     if not pt.exists(dst):
         os.mkdir(dst)
 
@@ -239,13 +267,15 @@ def sfr2vlm(src, dst, dim, ovr = False):
                 continue
             else:
                 renew = True
-        p_min = sf['pos'].min(axis = 0)
 
-        ## offset to 0, get voxel position
-        sf['slc'] = sf['slc'] > -1
-        sf = np.array(sf, dtype = VOX)
+        ## non-zero position
+        pos = [sf['pos'][a] for a in 'xyz'] 
+        vlm = np.zeros(dim, dtype = VLM)    # 3D volumn
+        for f in VLM.names:
+            vlm[f][pos]=sf[f]
+   
         with open(fo, 'wb') as pk:
-            cPickle.dump(sf, pk, cPickle.HIGHEST_PROTOCOL)
+            cPickle.dump(vlm, pk, cPickle.HIGHEST_PROTOCOL)
         
         if renew:
             print fo, "renewed"
@@ -292,18 +322,17 @@ def get_sf(src, si = 0, fv = 0, tv = None):
         print fi + ":"
         sf = cPickle.load(f)[fv:tv]
     return sf
-        
 
 def get_ps(src, si = 0):
     return get_sf(src, si)['pos']
 
 
 def test():
-#    csv2npy('dat/csv', 'dat/npy', ovr = 1, flt = lambda v: v[1] == 1011)
-#    vtx2grd('dat/npy', 'dat/vox', ovr = 1, gsz = 1)
-    srt_pos('dat/vox', 'dat/srt', ovr = 1)
-#    print get_dataset_bound("dat/agr")
-#    voffset("dat/agr", "dat/aln", ovr = 1, offset = 0)
+    csv2npy('dat/csv', 'dat/npy', ovr = 1, flt = lambda v: v[1] == 1011)
+    vtx2grd('dat/npy', 'dat/grd', ovr = 1, sz = 1)
+    srt_pos('dat/grd', 'dat/srt', ovr = 1)
+    cmb_pos('dat/srt', 'dat/cmb', ovr = 1)
+    sfr2vlm('dat/cmb', 'dat/vlm', ovr = 1, dim = (64,)*3)
     
 if __name__ == "__main__":
     test()
