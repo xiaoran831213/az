5b6a89f66c308de72e086582f6ec124f4af4e9ba
xtong <tongxia1@msu.edu>
2015-06-22 09:59:02 -0400

    work on surface extractor

--- cmd.sh
+++ cmd.sh
@@ -1,55 +1,60 @@
 #!/bin/bash
 
-sbj="002_S_0729"
 dir="$SUBJECTS_DIR"
 avg="$FREESURFER_HOME/subjects/fsaverage"
 
 ## extract surface vertices with respect to the the average brain
-for hm in {'lh','rh'}
+get_avg_vtx()
 {
-    ## extract area, curvature, convexity and thickness
-    for t in {'area','curv','sulc','thickness'}
-    {
-	## extract surface values and paint them to the average brain
-	fo="/tmp/$hm.$sbj.$t"
+    sbj=$1
+    for hm in {'lh','rh'}; do
+	## extract area, curvature, convexity and thickness
+	for t in {'area','curv','sulc','thickness'}; do
+	    ## extract surface values and paint them to the average brain
+	    fo="/tmp/$hm.$sbj.$t"
+	    if [ -e $fo ]; then
+		echo "xt: $fo exists."
+	    else
+ 		echo "xt: paint $t to $fo"
+		mri_surf2surf --hemi $hm --srcsubject $sbj --sval $t --src_type curv \
+		    --trgsubject fsaverage  --tval $fo --trg_type curv
+	    fi
+
+	    ## write the painted values to ASCII file
+	    if [ -e "$fo.asc" ]; then
+		echo "xt: $fo.asc exists."
+	    else
+ 		echo "xt: write $fo.asc"
+		mris_convert -c $fo $avg/surf/$hm.white "$fo.asc"
+	    fi
+	done
+
+	## 
+	fo="/tmp/$hm.$sbj.xyz"
 	if [ -e $fo ]; then
 	    echo "xt: $fo exists."
 	else
- 	    echo "xt: paint $t to $fo"
-	    mri_surf2surf --hemi $hm --srcsubject $sbj --sval $t --src_type curv \
-		--trgsubject fsaverage  --tval $fo --trg_type curv
+	    echo "xt: $fo <-- paint xyz to average $hm"
+	    mri_surf2surf --hemi $hm --srcsubject $sbj --sval-xyz white \
+		--trgsubject fsaverage  --tval-xyz --tval $fo
 	fi
 
-	## write the painted values to ASCII file
-	if [ -e "$fo.asc" ]; then
+	if [ -e $fo".asc" ]; then
 	    echo "xt: $fo.asc exists."
 	else
- 	    echo "xt: write $fo.asc"
-	    mris_convert -c $fo $avg/surf/$hm.white "$fo.asc"
+	    echo "xt: $fo.asc <-- write $fo to ascii"
+	    mris_convert $fo "$fo.asc"
 	fi
-    }
+    done
 
-    ## 
-    fo="/tmp/$hm.$sbj.xyz"
-    if [ -e $fo ]; then
-	echo "xt: $fo exists."
-    else
-	echo "xt: $fo <-- paint xyz to average $hm"
-	mri_surf2surf --hemi $hm --srcsubject $sbj --sval-xyz white \
-	    --trgsubject fsaverage  --tval-xyz --tval $fo
-    fi
-
-    if [ -e $fo".asc" ]; then
-	echo "xt: $fo.asc exists."
-    else
-	echo "xt: $fo.asc <-- write $fo to ascii"
-	mris_convert $fo "$fo.asc"
-    fi
+    n=$(sed $fo.asc -n -e '2,2p')
+    n=($n)
+    n_vtx=${n[0]}
+    n_tri=${n[1]}
     
-    n=$(wc -l </tmp/lh.002_S_0729.area.asc)
 }
 
-for i in ${!info[@]}
-{
-    echo $i ${info[i]}
-}
+# for i in ${!info[@]}
+# {
+#     echo $i ${info[i]}
+# }
