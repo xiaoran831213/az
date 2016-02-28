d4063288a54f71bdd7c0577dc5e674f6572d9b82
xtong <tongxia1@msu.edu>
2016-02-28 14:46:29 -0500

    try to plot the simulation result.

--- src/sim_rpt.R
+++ src/sim_rpt.R
@@ -20,19 +20,19 @@ pwr <- function(rpt, t = 0.05, ret=2)
     hdr <- names(rpt)
 
     ## symbolic patterns
-    pp <- '[#PFB]'                     # p-value correction
-    pd <- '[#BE][#0-9]'                # data type
-    pw <- '[#VGAX]'                    # u-kernel
-    pe <- '[#VGAXN][#LB]'              # effect
+    pp <- '[NF]'                        # p-value correction [NPFB]
+    pd <- '[BE][0-9]'                   # data type
+    pw <- '[VGAX]'                      # u-kernel
+    pe <- '[NVGAX][LB]'                 # effect
 
     ## pick out configurations
     cfg <- rpt[, !grepl(pe, hdr)]
 
     ## patten of effect symbol
     if(ret == 0)                        
-        pe <- '[#N][#LB]'
+        pe <- '[NN][NLB]'
     if(ret == 1)                   
-        pe <- '[#VGAX][#LB]'
+        pe <- '[NVGAX][NLB]'
 
     ## pattern of simulation symbol
     pts <- paste(pp, pd, pw, pe, sep='[.]')
@@ -125,41 +125,49 @@ pic <- function(pwr, xts='n.s', et='01234567', wt="VGX", yt="VGX")
 tab <- function(rr1)
 {
     rr1 <- na.omit(rr1)
-    
-    ## rv <- with(rr1, data.frame(sn=wsn, nm=wnm, sz=n.v, pvl=E4.V))
-    ## rv <- unique(rv)
-    ## rownames(rv) <- rv$sn
-    ## rv$sn <- NULL
-    ## rv$bon <- p.adjust(rv$pvl, 'bon')
-    ## rv$fdr <- p.adjust(rv$pvl, 'fdr')
-    ## rv <- with(rv, rv[order(pvl),])
-    ## ##write.csv(rv, '~/Dropbox/rv.csv', row.names = F)
 
-    ## rg <- with(rr1, data.frame(sn=gsn, nm=gnm, sz=n.g, pvl=E4.G))
-    ## rg <- unique(rg)
-    ## rownames(rg) <- rg$sn
-    ## rg$sn <- NULL
-    ## rg$bon <- p.adjust(rg$pvl, 'bon')
-    ## rg$fdr <- p.adjust(rg$pvl, 'fdr')
-    ## rg <- with(rg, rg[order(pvl),])
-    ## ##write.csv(rg, '~/Dropbox/rg.csv', row.names = F)
-    ##list(rv=rv, rg=rg, rx=rx)
-    rx <- with(rr1, data.frame(
-        row.names=paste(wsn, gsn, sep='.'), wnm, gnm, nv=n.v, ng=n.g,
-        pg=E4.G, pv=E4.V, px=E4.X))
-    rx <- unique(rx)
+    rv <- with(rr1, data.frame(sn=wsn, nm=wnm, sz=n.v, pvl=E4.V))
+    rv <- aggregate(formula = pvl ~ sn + nm + sz, FUN = mean, data = rv)
+    rownames(rv) <- rv$sn
+    rv$sn <- NULL
+    rv <- with(rv, rv[order(pvl),])
+    ##write.csv(rv, '~/Dropbox/rv.csv', row.names = F)
+    
+    rg <- with(rr1, data.frame(sn=gsn, nm=gnm, sz=n.g, pvl=E4.G))
+    rg <- aggregate(formula = pvl ~ sn + nm + sz, FUN = mean, data = rg)
+    rownames(rg) <- rg$sn
+    rg$sn <- NULL
+    rg <- with(rg, rg[order(pvl),])
+    ##write.csv(rg, '~/Dropbox/rg.csv', row.names = F)
+
+    rx <- with(
+        rr1,
+        data.frame(
+            row.names = paste(wsn, gsn, sep='.'),
+            wnm, gnm, nv=n.v, ng=n.g, pg=E4.G, pv=E4.V, px=E4.X))
     rx <- with(rx, rx[order(px),])
-    rx
+
+    list(rv=rv, rg=rg, rx=rx)
 }
 
-pix <- function(rx, pch=NULL, np = 1000)
+pix <- function(rx, pch=0, np = 1000)
 {
-    r2 <- rx[seq(1, nrow(rx), l=np),]
+    ## top 20
+    library(xtable)
+    tp20 <- subset(head(r1$rx, 20), select = c(wnm, gnm, pv, pg, px))
+    tp20 <- format(tp20, digits = 3)
+    names(tp20) <- c('cortical surface', 'gene', '$P_V$', '$P_G$', '$P_X$')
+    tp20 <- xtable(tp20, 'Top 20 combinations', 'tb:tp20')
+    print(tp20, file = 'rpt/tp20.tex', include.rownames = F, sanitize.text.function = identity)
+    
+    r2 <- rx[seq(1, nrow(rx), l=np), ]
     lpg <- -log10(r2$pg)
     lpv <- -log10(r2$pv)
     lpx <- -log10(r2$px)
 
-    png('lgp_xgv.png', width=1050, height=750, res=144)
+    ## library(tikzDevice)
+    ## tikz('rpt/q1a.fig.tex', width = 5, height = 5)
+    png('rpt/lgp_xgv.png', width=1050, height=750, res=144)
     yl <- '-LG(P)'
     yr <- c(0, max(lpg, lpv, lpx))
     par(pch = pch, cex.lab = 1.3, cex.axis = 1.3, mar = c(4,4,1,1), mgp = c(2.5, 1, 0))
@@ -175,4 +183,30 @@ pix <- function(rx, pch=NULL, np = 1000)
         legend=lgd, col=c('red', 'green', 'blue'), pch=pch, pt.cex = 1.5)
 
     dev.off()
+}
+
+## r1 <- readRDS('bin/rr1.rds')
+## pix(r1$rx)
+main <- function()
+{
+    ## load simulation output
+    d0 <- readRDS('dat/s4~8.rds')
+    d0 <- sapply(names(d0)[-(1:10)], function(x)
+    {
+        cf <- regexec('^(.)[.](..)[.](.)[.](.)(.)$', x)
+        cf <- regmatches(x, cf)[[1]][-1]
+        rt <- data.frame(
+            ssz = d0[, 4],              # sample size
+            vtx = cf[2],
+            knl = cf[3],
+            src = cf[4],
+            typ = cf[5],
+            adj = cf[1],
+            pvl = d0[, x],
+            stringsAsFactors = FALSE)
+        rt
+    }, simplify = F)
+
+    d1 <- do.call(rbind, d0)
+    d1
 }
